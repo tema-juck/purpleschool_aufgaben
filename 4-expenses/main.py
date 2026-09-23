478db224e903381f1737a3a9d829a6066bd650be
Daniel Temkin <date1002@stud.hs-kl.de>
2026-09-23 20:04:32 +0200

    6.17. Домашнее задание - Списки, кортежи

---
+++ 4-expenses/main.py
@@ -0,0 +1,9 @@
+weekend_budget = [25, 30, 15, 10, 20, 4, 45]
+
+sum = sum(weekend_budget)
+min = min(weekend_budget)
+max = max(weekend_budget)
+average = sum / len(weekend_budget)
+
+save = (min, max, sum)
+print(save)
