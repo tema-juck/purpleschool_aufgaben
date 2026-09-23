6c572a8ec789647f56ab7a580e495251d20f7c68
Daniel Temkin <date1002@stud.hs-kl.de>
2026-09-24 00:02:24 +0200

    7.11. Домашнее задание - Строки

--- 4-expenses/main.py
+++ 4-expenses/main.py
@@ -1,9 +1,22 @@
 money = input("Введите сумму в рублях: ").lower().strip()
 
-rubles = money.split("руб")[0].strip()
-kopecks = money.split("руб")[1].strip().split("коп")[0].strip()
+name_list = ["руб", "коп", "рубль", "рубля", "рублей", "копейка", "копейки", "копеек"]
 
-if not rubles.isdigit() or (kopecks and not kopecks.isdigit()):
+if  money.count("руб") != 1 or money.count("коп") != 1:
     print("Некорректный формат суммы")
+    exit()
+
+rubles = money.split("руб")[0].strip()
+
+kopecks = int(money.split("руб")[1].strip().split("коп")[0].strip())
+
+if kopecks > 99:
+    print("Некорректный формат суммы")
+    exit()
+
+if not rubles.isdigit():
+    print("Некорректный формат суммы")
+    exit()
 else:
-    print(f"{rubles}.{kopecks.zfill(2)} ₽")
+    print(f"{rubles}.{kopecks:02d} ₽")
+    exit()
